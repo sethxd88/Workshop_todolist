f570c65b5d01216e4863811e3dab48eb3bbb1061
seth <sepx898508@gmail.com>
2021-12-03 12:08:12 +0800

    Added feature: check and uncheck

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,18 @@
 // TO DO
+document.addEventListener("DOMContentLoaded", function () {
+
+  const checkList = document.querySelector('.ul_wrapper');
+
+  // console.log(checkList);
+  // console.log(checkBox.item);
+
+    checkList.addEventListener('click', function (event) {
+      const target = event.target;
+      if (target.classList.contains('checked') === false) {
+        // console.log(event.currentTarget);
+        target.classList.add('checked');
+      } else {
+        target.classList.remove('checked');
+      };
+    });
+	});
